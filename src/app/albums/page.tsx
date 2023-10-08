68b19fcbf6091c10e0a6ca5a19daa243765d188d
malt9830 <malt9830@stud.kea.dk>
2023-10-09 01:05:33 +0200

    Added albums page

---
+++ src/app/albums/page.tsx
@@ -0,0 +1,7 @@
+export default function Albums() {
+  return (
+    <main>
+      <h1>Albums</h1>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+    <main>
+      <h1>Main</h1>
     </main>
   )
 }
