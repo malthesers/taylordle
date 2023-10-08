5777934f542afdefecffdc7b215e5145ff8c88c6
malt9830 <malt9830@stud.kea.dk>
2023-10-09 01:49:59 +0200

    Updated header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,13 +4,14 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <header className="w-full p-4">
-      <div className="max-w-7xl mx-auto flex justify-between">
-        <Link href="/">taylordle</Link>
-        <nav>
+    <header className="w-full px-6 py-4 font-semibold">
+      <nav className="max-w-7xl mx-auto flex justify-between">
+        <Link href="/" className="text-xl">taylordle</Link>
+        <div className="flex gap-4">
+          <Link href="/">Game</Link>
           <Link href="/albums">Albums</Link>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </header>
   )
 }
