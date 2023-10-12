a7dff21ee2f63dd9a84cb6b4229abb028e8d29c5
malt9830 <malt9830@stud.kea.dk>
2023-10-12 03:28:33 +0200

    Now converts duration to mm:ss

--- src/components/game/SongRow.tsx
+++ src/components/game/SongRow.tsx
@@ -1,11 +1,14 @@
 import { Song } from "@/src/interfaces/song"
 
 export default function SongRow({ song }: { song: Song}) {
+  const min:number = Math.floor(song.duration / 60)
+  const sec:number = Math.floor(song.duration % 60)
+
   return (
     <div className="flex flex-row gap-4">
       <p>{song.album}</p>
       <p>{song.title}</p>
-      <p>{song.duration}</p>
+      <p>{min}:{sec}</p>
     </div>
   )
 }
