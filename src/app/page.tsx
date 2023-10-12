d8c9032352993ab577f379cf6d49747eb4a72a48
malt9830 <malt9830@stud.kea.dk>
2023-10-12 02:16:58 +0200

    Added Song interface and type for songs state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+"use client"
+
+import { useState } from "react"
+
+interface Song {
+  no: number,
+  title: string,
+  album: string,
+  length: number
+}
+
 export default function Home() {
+
+  const [songs, setSongs] = useState<Array<Song>>([
+    {
+      no: 1,
+      title: "Tim McGraw",
+      album: "Taylor Swift",
+      length: 234
+    },
+    {
+      no: 2,
+      title: "Picture to Burn",
+      album: "Taylor Swift",
+      length: 175
+    }
+  ])
+
   return (
     <main>
       <h1>Main</h1>
+      { songs.map((song) =>
+        <p key={song.title}>{song.title}</p>
+      )}
     </main>
   )
 }
